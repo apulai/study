c9e90c2518953563912eeb3586fced9d985f4493
pa <andras.pulai@gmail.com>
2019-11-20 16:57:24 +0100

    Completed mimimumconcat challange for Turing.

--- minimumConcat.py
+++ minimumConcat.py
@@ -1,53 +1,50 @@
 def minimumConcat(source, target):
     # Put your code here
     #map = new Map();
-    mydict = {}
 
-    for i in range(len(source)):
-        if(mydict(source[i])):
-            arr=map(source[i])
+    # We will generate all substrings
+    # These will placed in a dictionary like word:length
+    # We will always take out words from the dictionary and try to delete 1 letter from it at each postion
+    # if the new word is not in the dict yet we will add it
+    # we are finished once no new additions made
 
+    dict = {source:len(source)}
+    couldaddnewword = True
+    while couldaddnewword== True:
+        couldaddnewword=False
+        # list(dict)
+        # if we are not using this form we will get a dictionary changed during iteration
+        for word in list(dict):
+            for i in range(len(word)):
+                newword = word.replace(word[i],"",1)
+                if newword not in dict:
+                    dict[newword]=len(newword)
+                    couldaddnewword = True
 
-    for (let i = 0; i < source.length; i++) {
-    if (map.get(source[i])) {
-    const arr = map.get(source[i]);
-    arr.push(i);
-    map.set(source[i], arr);
-    } else {
-    map.set(source[i], [i]);
-    }
-    }
-    let
-    occurrences = 0;
-    for (let i = 0; i < target.length; i++) {
-        const indexes = map.get(target[i]);
-    if (indexes == = void 0)
-    return -1;
-    occurrences + +;
-    let
-    max = 0;
-    indexes.forEach(index= > {
-        let
-    j = 0;
-    let
-    ignore = 0;
-    while (source[index + j + ignore] !== void 0) {
-    if (target[i + j] != = source[index + j + ignore]) {
-    ignore++;
-    } else {
-    max = Math.max(max, j++);
-    }
-    }
-    });
-    i += max;
-    }
-    return occurrences;
+    print(dict)
 
+    replacements=0
+    for i in sorted(dict.items(), key=lambda x: (-x[1],x[0])):
+        print(i,i[0])
+        newtgt=target.replace(i[0],"")
+        if not newtgt==target:
+            replacements += 1
+            target=newtgt
+            print(target, end=" ")
+        print(target)
 
+    if( len(target) > 0):
+        return -1
+    else:
+        return replacements
 
+#source = input()
+#target = input()
+source="abcd"
+target="abcbdabcdd"
+rv=minimumConcat(source, target)
 
-#initial = input()
-#goal = input()
-initial="xyz"
-goal="xytzxz"
-minimumConcat(initial, goal)
+if rv!=-1:
+    print("To create {} from {} we will need {} substrings of {}".format(target,source,rv,source))
+else:
+    print("It is not possible to create {} from {}".format(target,srouce))
